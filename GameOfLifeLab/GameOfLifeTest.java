419ac44e43e2cafec030afe0660872a28c29e859
AllenLin001 <allenlin41@yahoo.com>
2014-11-28 07:02:55 -0600

    Done Please Grade this one!!

--- GameOfLifeLab/GameOfLifeTest.java
+++ GameOfLifeLab/GameOfLifeTest.java
@@ -91,7 +91,7 @@ public class GameOfLifeTest
     @Test
     public void testFinalState()
     {
-        /* verify that the actual pattern matches the expected pattern after 3 generations         *  
+        /* verify that the actual pattern matches the expected pattern after 6 generations         *  
          *
          *  (X: alive; -: dead)
          * 
